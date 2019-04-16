ea96b117a561aa214cfaf68edbeb206a4342ade7
huxiubin <490101299@qq.com>
2019-04-16 23:03:22 +0800

    1

--- docs/config/config.js
+++
@@ -1,9 +0,0 @@
-export const loginInfo = {
-  user: 'hxb',
-  pwd: '123456'
-};
-
-export const BASE_URL = 'http://localhost:8200/'; //'https://family.1ziton.com/';
-
-export const USER_COOKIE = '_user_cookie_';
-export const USER_INFO = '_user_info_';
